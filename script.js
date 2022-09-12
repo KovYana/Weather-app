df9929660f0980dc4d480a1b4a8ed17694d9fc2f
KovYana <rybak.yana@gmail.com>
2022-09-12 14:46:54 +0200

    Added JS file

---
+++ script.js
@@ -0,0 +1,62 @@
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[now.getDay()];
+
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minute = now.getMinutes();
+if (minute < 10) {
+  minute = `0${minute}`;
+}
+let date = document.querySelector("#date-and-time");
+date.innerHTML = `${day}, ⌚${hour}:${minute}`;
+
+function showTemp(response) {
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#temperature-characterictic").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  document.querySelector("#wind").innerHTML = `Wind: ${Math.round(
+    response.data.wind.speed
+  )} km/h`;
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input");
+  let h1 = document.querySelector("#current-city");
+  h1.innerHTML = city.value;
+  let apiKey = "36c58b321eebc67821aa228dc67d4641";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+function searchLocation(position) {
+  let apiKey = "36c58b321eebc67821aa228dc67d4641";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+let searchInput = document.querySelector("#search-form");
+searchInput.addEventListener("submit", searchCity);
